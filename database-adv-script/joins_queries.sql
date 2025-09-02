91f18d84696922401d74b0da859bc812ecbc4858
Abdellah Hadid <141076379+Fokoda799@users.noreply.github.com>
2025-09-02 08:35:23 +0100

    Create joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,22 @@
+SELECT 
+    booking.booking_id,
+    users.first_name,
+    users.last_name,
+    users.email,
+    booking.start_date,
+    booking.end_date,
+    booking.total_price,
+    booking.status
+FROM booking
+INNER JOIN users
+ON booking.user_id = users.user_id;
+
+SELECT * 
+FROM property 
+LEFT JOIN review 
+ON review.property_id = property.property_id;
+
+SELECT *
+FROM users
+FULL JOIN booking
+ON booking.user_id = users.user_id;
